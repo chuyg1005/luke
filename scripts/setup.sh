4648c54d7708eec38bd346a4f979f018c1d8c008
yukang <ygchu7@163.com>
2024-04-25 12:19:18 +0800

    update filepath refers in scripts/setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,9 @@
+data_dir='../data'
+results_dir='../results'
 rm -rf data
 rm -rf results
-ln -s /home/data_91_d/chuyg/mix-debias/data data
-ln -s /home/data_91_d/chuyg/mix-debias/luke/results results
+ln -s ${data_dir} data
+if [ ! -d "${results_dir}/luke/results" ]; then
+    mkdir -p ${results_dir}/luke/results
+fi
+ln -s ${results_dir}/luke/results results
